1dcc1a4f376a56e35c13202f8c33c84ad51af888
Shabex <shabsibrah@gmail.com>
2022-06-04 13:23:47 +0300

    commit tree cde605ae4a12897d54dfdd1c1afca9d98e93c362

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const name = ["Guadalupe","Ollie", "Aki"];
+function writeCards(name) {
+    let messages = [];
+for (let i=0; i<name.length; i++){
+    messages.push (`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+}
+return messages;
+}
+ 
+let decrement = 10; 
+function countDown (decrement){
+   while (decrement >= 0) {
+       console.log(decrement);
+       decrement -= 1;
+   }  
+}
+countDown(decrement);
